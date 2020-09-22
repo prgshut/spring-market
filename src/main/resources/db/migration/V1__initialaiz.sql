b7f4bd6ce5ded4e014d92a857558f5fcf736cb61
zheleznovdv <prgshut@gmail.com>
2020-09-22 07:58:25 +0300

    ini

--- src/main/resources/db/migration/V1__initialaiz.sql
+++ src/main/resources/db/migration/V1__initialaiz.sql
@@ -1,5 +1,5 @@
-CREATE TABLE `shop`.`products` (
-  `id`      INT(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `products` (
+  `id`      INT NOT NULL AUTO_INCREMENT,
   `title`   VARCHAR(255) NOT NULL,
   `price`   FLOAT NOT NULL,
   PRIMARY KEY (`id`))
