eb004d7bf7fe1293d790de09041fbcdea90596d8
zheleznovdv <prgshut@gmail.com>
2020-09-23 15:37:33 +0300

    les6

--- src/main/java/com/geek/market/controllers/MainController.java
+++ src/main/java/com/geek/market/controllers/MainController.java
@@ -1,13 +1,18 @@
 package com.geek.market.controllers;
 
+import com.geek.market.entities.Product;
 import com.geek.market.services.ProductService;
+import com.geek.market.utils.ProductFilter;
 import lombok.AllArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 @AllArgsConstructor
@@ -18,27 +23,14 @@ public class MainController {
     @GetMapping
     public String productAll(Model model,
                              @RequestParam(defaultValue = "1", name = "p") Integer page,
-                             @RequestParam(required = false, defaultValue = "0") int min,
-                             @RequestParam(required = false,defaultValue = "0") int max
-    ) {
+                             @RequestParam(required = false)Map<String,String> param
+                             ) {
         if (page < 1) {
             page = 1;
         }
-        if (min != 0 && max != 0) {
-            model.addAttribute("products",
-                    productService.findByPriceGreaterThanEqualAndPriceLessThan(page-1,5,max, min));
-            return "products";
-        }
-        if (min > 0 && max == 0) {
-            model.addAttribute("products", productService.findByPriceGreaterLessThan(page-1,5,min));
-            return "products";
-        }
-        if (min == 0 && max > 0) {
-            model.addAttribute("products", productService.findByPriceGreaterThanEqual(page-1,5,max));
-            return "products";
-        }
-
-            model.addAttribute("products", productService.findAll(page - 1, 5));
+        ProductFilter productFilter = new ProductFilter(param);
+        Specification<Product> spec = productFilter.
+            model.addAttribute("products", productService.findAll(page - 1, 5, spec));
             return "products";
 
     }
--- src/main/java/com/geek/market/services/ProductService.java
+++ src/main/java/com/geek/market/services/ProductService.java
@@ -5,30 +5,18 @@ import com.geek.market.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class ProductService {
     private ProductRepository productRepository;
 
-    public Page<Product> findAll(int page, int size) {
-        return productRepository.findAll(PageRequest.of(page, size));
+    public Page<Product> findAll(int page, int size, Specification<Product> spec) {
+        return productRepository.findAll(spec, PageRequest.of(page, size));
     }
 
-    public Page<Product> findByPriceGreaterThanEqualAndPriceLessThan(int page, int size, int min, int max) {
-        return productRepository.findByPriceGreaterThanEqualAndPriceLessThanEqual(PageRequest.of(page, size), max, min);
-    }
-
-    public Page<Product> findByPriceGreaterThanEqual(int page, int size, int max) {
-        return productRepository.findByPriceGreaterThanEqual(PageRequest.of(page, size),max);
-    }
-
-    public Page<Product> findByPriceGreaterLessThan(int page, int size, int min) {
-        return productRepository.findByPriceLessThanEqual(PageRequest.of(page, size),min);
-    }
 
 }
